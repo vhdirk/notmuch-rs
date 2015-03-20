cba797fea1d9bf9d4b12b991b73a11b6d082faed
C. Morgan Hamill <me@cmhamill.org>
2015-03-20 10:55:05 -0400

    Add `Database::create()` method.
    Simple wrapper around notmuch API's `notmuch_database_create()`
    function.

--- src/database.rs
+++ src/database.rs
@@ -1,4 +1,12 @@
+use std::{
+    path,
+    ptr,
+};
+
 use libc;
+
+use error::Result;
+use utils::ToCString;
 
 use ffi;
 
@@ -9,3 +17,16 @@ pub use ffi::NotmuchDatabaseMode as Mode;
 pub struct Version(libc::c_uint);
 
 pub struct Database(*mut ffi::notmuch_database_t);
+
+impl Database {
+    pub fn create<P: path::AsPath>(path: &P) -> Result<Database> {
+        let path = path.as_path().to_cstring().unwrap();
+
+        let mut db = ptr::null_mut();
+        try!(unsafe {
+            ffi::notmuch_database_create(path.as_ptr(), &mut db)
+        }.as_result());
+
+        Ok(Database(db))
+    }
+}
