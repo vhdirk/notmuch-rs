64304118fcd8a876815450194a315846b1a95422
Dirk Van Haerenborgh <vhdirk@gmail.com>
2018-03-22 09:56:54 +0100

    correct lifetime for query

--- src/directory.rs
+++ src/directory.rs
@@ -1,4 +1,5 @@
 use std::{
+    ops,
     marker,
 };
 
@@ -18,5 +19,13 @@ pub struct Directory<'d>(
 impl<'d> NewFromPtr<*mut ffi::notmuch_directory_t> for Directory<'d> {
     fn new(ptr: *mut ffi::notmuch_directory_t) -> Directory<'d> {
         Directory(ptr, marker::PhantomData)
+    }
+}
+
+impl<'d> ops::Drop for Directory<'d> {
+    fn drop(&mut self) {
+        unsafe {
+            ffi::notmuch_directory_destroy(self.0)
+        };
     }
 }
--- src/query.rs
+++ src/query.rs
@@ -1,24 +1,38 @@
 use std::{
     ops,
+    marker
 };
 
 use error::Result;
 
 use ffi;
+use utils::{
+    NewFromPtr,
+};
+use Database;
 
-use database::Database;
 
 #[derive(Debug)]
-pub struct Query(pub(crate) *mut ffi::notmuch_query_t);
+pub struct Query<'d>(
+    pub(crate) *mut ffi::notmuch_query_t,
+    marker::PhantomData<&'d mut Database>,
+);
 
-impl Query {
-    pub fn create(db: &Database, query_string: &String) -> Result<Self> {
+
+impl<'d> Query<'d> {
+    pub fn create(db: &'d Database, query_string: &String) -> Result<Self> {
         db.create_query(query_string)
     }
 }
 
+impl<'d> NewFromPtr<*mut ffi::notmuch_query_t> for Query<'d> {
+    fn new(ptr: *mut ffi::notmuch_query_t) -> Query<'d> {
+        Query(ptr, marker::PhantomData)
+    }
+}
 
-impl ops::Drop for Query {
+
+impl<'d> ops::Drop for Query<'d> {
     fn drop(&mut self) {
         unsafe {
             ffi::notmuch_query_destroy(self.0)
