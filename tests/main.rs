b48156d31489e4ee18bae0da99484792d9568faf
Dirk Van Haerenborgh <vhdirk@gmail.com>
2018-10-06 12:19:45 +0200

    fix test for older notmuch version

--- tests/main.rs
+++ tests/main.rs
@@ -8,9 +8,12 @@ fn main() {
     match notmuch::Database::open(&mail_path.to_str().unwrap().to_string(), notmuch::DatabaseMode::ReadOnly){
         Ok(db) => {
             
-            let rev = db.revision();
-            println!("db revision: {:?}", rev);
-
+            #[cfg(feature = "v0_21")]
+            {
+                let rev = db.revision();
+                println!("db revision: {:?}", rev);
+            }
+            
             let query = db.create_query(&"".to_string()).unwrap();
             let mut threads = query.search_threads().unwrap();
 
